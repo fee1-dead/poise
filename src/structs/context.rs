4d3bfa50e23aca80570f7454dd8b585ef58713b5
GnomedDev <david2005thomas@gmail.com>
2023-12-10 00:11:33 +0000

    Fix breakage due to FixedString

--- src/structs/context.rs
+++ src/structs/context.rs
@@ -450,7 +450,7 @@ context_methods! {
                 }
                 string
             }
-            Context::Prefix(ctx) => ctx.msg.content.clone(),
+            Context::Prefix(ctx) => ctx.msg.content.to_string(),
         }
     }
 
